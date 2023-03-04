55c651613be3984918780536dc78b6e7d007d58c
skesswswkk <skesswswkk@naver.com>
2023-03-04 17:59:37 +0900

    init

--- app/src/androidTest/java/app/youtubeapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/youtubeapplication/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("youtubeapplication", appContext.packageName)
+        assertEquals("app.youtubeapplication", appContext.packageName)
     }
 }
